0f6186751276e0edf05f97307b3d14736efc3982
aluno <beatrizds.unifeg.edu.br>
2019-05-14 17:24:14 -0300

    Programa de soma

---
+++ PrimeiroSemestre/Calculo1/soma.py
@@ -0,0 +1,9 @@
+#Programa simples que pede a soma de dois numeros e exibe o resultado
+
+print("digite o numero1")
+n1 = int(input())
+
+print("digite o numero 2")
+n2 = int(input())
+
+print("resultado: ",n1+n2
